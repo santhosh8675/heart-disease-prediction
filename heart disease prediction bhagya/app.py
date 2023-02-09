90b6e7e97c3c0340c7113b6f9f03f22f23e3795d
Santhosh sivan <83957480+santhosh8675@users.noreply.github.com>
2023-02-09 12:55:29 +0530

    Add files via upload

---
+++ heart disease prediction bhagya/app.py
@@ -0,0 +1,147 @@
+import streamlit as st
+import pandas as pd
+import polars as pl
+import numpy as np
+import pickle
+import sklearn
+
+DATASET_PATH = "data/heart_2020_cleaned.csv"
+LOG_MODEL_PATH = "model/model_heart.pkl"
+
+
+def main():
+    @st.cache(persist=True)
+    def load_dataset() -> pd.DataFrame:
+        heart_df = pl.read_csv(DATASET_PATH)
+        heart_df = heart_df.to_pandas()
+        heart_df = pd.DataFrame(np.sort(heart_df.values, axis=0),
+                                index=heart_df.index,
+                                columns=heart_df.columns)
+        return heart_df
+
+
+    def user_input_features() -> pd.DataFrame:
+        race = st.sidebar.selectbox("Race", options=(race for race in heart.Race.unique()))
+        sex = st.sidebar.selectbox("Sex", options=(sex for sex in heart.Sex.unique()))
+        age_cat = st.sidebar.selectbox("Age category",
+                                       options=(age_cat for age_cat in heart.AgeCategory.unique()))
+        bmi_cat = st.sidebar.selectbox("BMI category",
+                                       options=(bmi_cat for bmi_cat in heart.BMICategory.unique()))
+        sleep_time = st.sidebar.number_input("How many hours on average do you sleep?", 0, 24, 7)
+        gen_health = st.sidebar.selectbox("How can you define your general health?",
+                                          options=(gen_health for gen_health in heart.GenHealth.unique()))
+        phys_health = st.sidebar.number_input("For how many days during the past 30 days was"
+                                              " your physical health not good?", 0, 30, 0)
+        ment_health = st.sidebar.number_input("For how many days during the past 30 days was"
+                                              " your mental health not good?", 0, 30, 0)
+        phys_act = st.sidebar.selectbox("Have you played any sports (running, biking, etc.)"
+                                        " in the past month?", options=("No", "Yes"))
+        smoking = st.sidebar.selectbox("Have you smoked at least 100 cigarettes in"
+                                       " your entire life (approx. 5 packs)?)",
+                                       options=("No", "Yes"))
+        alcohol_drink = st.sidebar.selectbox("Do you have more than 14 drinks of alcohol (men)"
+                                             " or more than 7 (women) in a week?", options=("No", "Yes"))
+        stroke = st.sidebar.selectbox("Did you have a stroke?", options=("No", "Yes"))
+        diff_walk = st.sidebar.selectbox("Do you have serious difficulty walking"
+                                         " or climbing stairs?", options=("No", "Yes"))
+        diabetic = st.sidebar.selectbox("Have you ever had diabetes?",
+                                        options=(diabetic for diabetic in heart.Diabetic.unique()))
+        asthma = st.sidebar.selectbox("Do you have asthma?", options=("No", "Yes"))
+        kid_dis = st.sidebar.selectbox("Do you have kidney disease?", options=("No", "Yes"))
+        skin_canc = st.sidebar.selectbox("Do you have skin cancer?", options=("No", "Yes"))
+
+        features = pd.DataFrame({
+            "PhysicalHealth": [phys_health],
+            "MentalHealth": [ment_health],
+            "SleepTime": [sleep_time],
+            "BMICategory": [bmi_cat],
+            "Smoking": [smoking],
+            "AlcoholDrinking": [alcohol_drink],
+            "Stroke": [stroke],
+            "DiffWalking": [diff_walk],
+            "Sex": [sex],
+            "AgeCategory": [age_cat],
+            "Race": [race],
+            "Diabetic": [diabetic],
+            "PhysicalActivity": [phys_act],
+            "GenHealth": [gen_health],
+            "Asthma": [asthma],
+            "KidneyDisease": [kid_dis],
+            "SkinCancer": [skin_canc]
+        })
+
+        return features
+
+
+    st.set_page_config(
+        page_title="Heart Disease Prediction App",
+        page_icon="images/heart-fav.png"
+    )
+
+    st.title("Heart Disease Prediction")
+    st.subheader("Are you worried about the condition of your heart? "
+                 "This app will help you to diagnose it!")
+
+    col1, col2 = st.columns([1, 3])
+
+    with col1:
+        st.image("images/doctor.png",
+                 caption="I'll help you diagnose your heart health!",
+                 width=150)
+        submit = st.button("Predict")
+    with col2:
+        st.markdown("""
+        In this app, you can
+        estimate your chance of heart disease (yes/no) in seconds!
+        
+        To predict your heart disease status, simply follow the steps bellow:
+        1. Enter the parameters that best describe you;
+        2. Press the "Predict" button and wait for the result.
+            
+        **Keep in mind that this results is not equivalent to a medical diagnosis!
+        This model would never be adopted by health care facilities because of its less
+        than perfect accuracy, so if you have any problems, consult a human doctor.**
+        
+        """)
+
+    heart = load_dataset()
+
+    st.sidebar.title("Feature Selection")
+    st.sidebar.image("images/heart-sidebar.png", width=100)
+
+    input_df = user_input_features()
+    df = pd.concat([input_df, heart], axis=0)
+    df = df.drop(columns=["HeartDisease"])
+
+    cat_cols = ["BMICategory", "Smoking", "AlcoholDrinking", "Stroke", "DiffWalking",
+                "Sex", "AgeCategory", "Race", "Diabetic", "PhysicalActivity",
+                "GenHealth", "Asthma", "KidneyDisease", "SkinCancer"]
+    for cat_col in cat_cols:
+        dummy_col = pd.get_dummies(df[cat_col], prefix=cat_col)
+        df = pd.concat([df, dummy_col], axis=1)
+        del df[cat_col]
+
+    df = df[:1]
+    df.fillna(0, inplace=True)
+
+    log_model = pickle.load(open(LOG_MODEL_PATH, "rb"))
+
+    if submit:
+        prediction = log_model.predict(df)
+        prediction_prob = log_model.predict_proba(df)
+        if prediction == 0:
+            st.markdown(f"**The probability that you'll have"
+                        f" heart disease is {round(prediction_prob[0][1] * 100, 2)}%."
+                        f" You are healthy!**")
+            st.image("images/vec.jpg",
+                     caption="Your heart seems to be okay! ")
+        else:
+            st.markdown(f"**The probability that you will have"
+                        f" heart disease is {round(prediction_prob[0][1] * 100, 2)}%."
+                        f" It sounds like you are not healthy.**")
+            st.image("images/heart-bad.jpg",
+                     caption="I'm not satisfied with the condition of your heart!")
+
+
+if __name__ == "__main__":
+    main()
